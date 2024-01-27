e06e3742f64368929e84d106a1a1f3a3e30b1ae0
ikehiro1 <ikegami0922hiroki@gmail.com>
2024-01-27 14:37:54 +0900

    java

---
+++ Index.java
@@ -0,0 +1,27 @@
+    //ファイル名と同じ
+class Index{
+    public static void main(String[] args){
+        System.out.println("おはよう");
+
+        //型  //変数
+        int kazu = 1;
+        int kazu_0 = 2;
+        int suuji = 3;
+
+        //
+        System.out.println(kazu);
+        System.out.println(kazu_0);
+        System.out.println(suuji);
+        
+        byte var01 = 2;
+        short var_01 = 12345;
+        int a001 = 1234567890;
+        long a$ = 1234567890000L;
+
+        System.out.println("数字は"+var01);
+        System.out.println(var_01);
+        System.out.println(a001);
+        System.out.println(a$+"です");
+
+    }
+}
